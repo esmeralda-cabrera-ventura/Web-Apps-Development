b49d73167a81db8ccd4d7a7e8c8d41510ff778f8
Esmeralda Cabrera Ventura <160828383+esmeralda-cabrera-ventura@users.noreply.github.com>
2026-03-29 01:20:55 -0700

    Delete ACME-Parking-Phase-4/make_hash.php

--- ACME-Parking-Phase-4/make_hash.php
+++
@@ -1,5 +0,0 @@
-<?php
-echo "ADMIN: " . password_hash("Admin123!", PASSWORD_BCRYPT) . "<br>";
-echo "VALET: " . password_hash("Valet123!", PASSWORD_BCRYPT) . "<br>";
-echo "ATTENDANT: " . password_hash("Attend123!", PASSWORD_BCRYPT) . "<br>";
-?>
